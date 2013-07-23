4e640871dbafd2efb7cc1ac8bd55b26496d1a5c2
Denis Zalevskiy <denis.zalevskiy@jolla.com>
2013-07-24 02:29:31 +0300

    [subscriber] using separate thread for polling
    Properties are monitored in a separate thread because during animation
    main thread polls 'em frequently
    
    Signed-off-by: Denis Zalevskiy <denis.zalevskiy@jolla.com>

--- src/property.cpp
+++ src/property.cpp
@@ -5,6 +5,344 @@
 #include <QDebug>
 #include <QTimer>
 #include <QSocketNotifier>
+#include <QMutex>
+
+namespace ckit
+{
+
+static QMutex actorGuard;
+static ckit::Actor<ckit::PropertyMonitor> *propertyMonitor = nullptr;
+static bool isActorCreated = false;
+
+Event::Event(Event::Type t)
+    : QEvent(static_cast<QEvent::Type>(t))
+{}
+
+Event::~Event() {}
+PropertyRequest::~PropertyRequest() {}
+
+PropertyRequest::PropertyRequest(Event::Type t
+                                 , ContextPropertyPrivate const *tgt
+                                 , QString const &key)
+    : Event(t)
+    , tgt_(tgt)
+    , key_(key)
+{}
+
+bool PropertyMonitor::event(QEvent *e)
+{
+    if (e->type() < QEvent::User)
+        return QObject::event(e);
+
+    auto t = static_cast<Event::Type>(e->type());
+    switch (t) {
+    case Event::Subscribe: {
+        auto p = static_cast<PropertyRequest*>(e);
+        subscribe(p->tgt_, p->key_);
+        break;
+    }
+    case Event::Unsubscribe: {
+        auto p = static_cast<PropertyRequest*>(e);
+        unsubscribe(p->tgt_, p->key_);
+        break;
+    }
+    default:
+        return QObject::event(e);
+    }
+    return true;
+}
+
+void PropertyMonitor::subscribe(ContextPropertyPrivate const *tgt, const QString &key)
+{
+    CKitProperty *handler;
+    auto it = targets_.find(key);
+    if (it == targets_.end()) {
+        targets_.insert(key, QSet<ContextPropertyPrivate const*>({tgt}));
+        handler = add(key);
+    } else {
+        if (it->contains(tgt)) {
+            return;
+        }
+        it->insert(tgt);
+        handler = properties_[key];
+    }
+    connect(handler, &CKitProperty::changed, tgt, &ContextPropertyPrivate::changed);
+}
+
+void PropertyMonitor::unsubscribe
+(ContextPropertyPrivate const *tgt, const QString &key)
+{
+    auto t_it = targets_.find(key);
+    if (t_it == targets_.end())
+        return;
+
+    auto tgt_set = t_it.value();
+    auto ptgt = tgt_set.find(tgt);
+    if (ptgt == tgt_set.end())
+        return;
+    
+    auto h_it = properties_.find(key);
+    if (h_it == properties_.end())
+        return;
+
+    auto handler = h_it.value();
+
+    disconnect(handler, &CKitProperty::changed
+               , tgt, &ContextPropertyPrivate::changed);
+    tgt_set.erase(ptgt);
+    if (!tgt_set.isEmpty())
+        return;
+
+    targets_.erase(t_it);
+    handler->deleteLater();
+    properties_.erase(h_it);
+}
+
+CKitProperty* PropertyMonitor::add(const QString &key)
+{
+    auto it = properties_.insert(key, new CKitProperty(key, this));
+    return it.value();
+}
+
+CKitProperty::CKitProperty(const QString &key, QObject *parent)
+    : QObject(parent)
+    , key_(key)
+    , file_(getStateFsPath(key))
+    , notifier_(nullptr)
+    , reopen_interval_(100)
+    , reopen_timer_(new QTimer(this))
+    , is_subscribed_(false)
+    , is_cached_(false)
+{
+    reopen_timer_->setSingleShot(true);
+    connect(reopen_timer_, SIGNAL(timeout()), this, SLOT(trySubscribe()));
+    subscribe();
+}
+
+CKitProperty::~CKitProperty()
+{
+    unsubscribe();
+}
+
+void CKitProperty::trySubscribe() const
+{
+    if (tryOpen()) {
+        reopen_interval_ = 100;
+        return subscribe();
+    }
+
+    reopen_interval_ *= 2;
+    if (reopen_interval_ > 1000 * 60 * 3)
+        reopen_interval_ = 1000 * 60 * 3;
+
+    reopen_timer_->start(reopen_interval_);
+}
+
+void CKitProperty::resubscribe() const
+{
+    bool was_subscribed = is_subscribed_;
+    unsubscribe();
+
+    if (was_subscribed)
+        subscribe();
+}
+
+QVariant CKitProperty::value() const
+{
+    QVariant res;
+
+    if (is_cached_)
+        return cache_;
+
+    if (!tryOpen()) {
+        qWarning() << "Can't open " << file_.fileName();
+        return res;
+    }
+
+    // WORKAROUND: file is just opened and closed before reading from
+    // real source to make vfs (?) reread file data to cache
+    QFile touchFile(file_.fileName());
+    touchFile.open(QIODevice::ReadOnly | QIODevice::Unbuffered);
+
+    file_.seek(0);
+    auto size = file_.size();
+    if (buffer_.size() < size)
+        buffer_.resize(size + 8);
+
+    int rc = file_.read(buffer_.data(), size + 7);
+    touchFile.close();
+    if (rc >= 0) {
+        buffer_[rc] = '\0';
+        auto s = QString(buffer_);
+        if (s.size()) // use read data if not empty
+            res = cKitValueDecode(s);
+
+        cache_ = res;
+        is_cached_ = true;
+
+        if (notifier_)
+            notifier_->setEnabled(true);
+    } else {
+        qWarning() << "Error accessing? " << rc << "..." << file_.fileName();
+        resubscribe();
+    }
+    return res;
+}
+
+void CKitProperty::handleActivated(int)
+{
+    if (notifier_)
+        notifier_->setEnabled(false);
+    is_cached_ = false;
+    emit changed(value());
+}
+
+bool CKitProperty::tryOpen() const
+{
+    if (file_.isOpen())
+        return true;
+
+    if (!file_.exists()) {
+        qWarning() << "No property file " << file_.fileName();
+        return false;
+    }
+    file_.open(QIODevice::ReadOnly);// | QIODevice::Unbuffered);
+    if (!file_.isOpen()) {
+        qWarning() << "Can't open " << file_.fileName();
+        return false;
+    }
+    is_cached_ = false;
+    return true;
+}
+
+void CKitProperty::subscribe() const
+{
+    if (is_subscribed_)
+        return;
+
+    is_subscribed_ = true;
+    if (!tryOpen())
+        return reopen_timer_->start(reopen_interval_);
+
+    notifier_.reset(new QSocketNotifier(file_.handle(), QSocketNotifier::Read));
+    connect(notifier_.data(), SIGNAL(activated(int))
+            , this, SLOT(handleActivated(int)));
+    notifier_->setEnabled(true);
+}
+
+void CKitProperty::unsubscribe() const
+{
+    if (!is_subscribed_)
+        return;
+
+    is_subscribed_ = false;
+    if (!file_.isOpen())
+        return;
+
+    if (notifier_) {
+        notifier_->setEnabled(false);
+        notifier_.reset();
+    }
+    file_.close();
+}
+
+}
+
+
+ContextPropertyPrivate::ContextPropertyPrivate(const QString &key, QObject *parent)
+    : QObject(parent)
+    , key_(key)
+    , is_subscribed_(false)
+    , is_cached_(false)
+{
+    subscribe();
+}
+
+ContextPropertyPrivate::~ContextPropertyPrivate()
+{
+    unsubscribe();
+}
+
+QString ContextPropertyPrivate::key() const
+{
+    return key_;
+}
+
+QVariant ContextPropertyPrivate::value(const QVariant &defVal) const
+{
+    return is_cached_ ? cache_ : defVal;
+}
+
+QVariant ContextPropertyPrivate::value() const
+{
+    return value(QVariant());
+}
+
+ckit::Actor<ckit::PropertyMonitor> * ContextPropertyPrivate::actor()
+{
+    using namespace ckit;
+    if (isActorCreated)
+        return propertyMonitor;
+    QMutexLocker lock(&ckit::actorGuard);
+    if (isActorCreated)
+        return propertyMonitor;
+    propertyMonitor = new ckit::Actor<ckit::PropertyMonitor>([=]() {
+            return new ckit::PropertyMonitor();
+        });
+    isActorCreated = true;
+    return propertyMonitor;
+}
+ 
+void ContextPropertyPrivate::changed(QVariant v)
+{
+    if (v.isNull() || (is_cached_ && v == cache_))
+        return;
+    is_cached_ = true;
+    cache_ = v;
+    emit valueChanged();
+}
+
+
+const ContextPropertyInfo* ContextPropertyPrivate::info() const
+{
+    return nullptr; // TODO
+}
+
+void ContextPropertyPrivate::subscribe() const
+{
+    if (is_subscribed_)
+        return;
+
+    actor()->postEvent(new ckit::PropertyRequest(ckit::Event::Subscribe, this, key_));
+    QCoreApplication::processEvents();
+    is_subscribed_ = true;
+}
+
+void ContextPropertyPrivate::unsubscribe() const
+{
+    if (!is_subscribed_)
+        return;
+
+    actor()->postEvent(new ckit::PropertyRequest(ckit::Event::Unsubscribe, this, key_));
+    is_subscribed_ = false;
+}
+
+void ContextPropertyPrivate::waitForSubscription() const
+{
+}
+
+void ContextPropertyPrivate::waitForSubscription(bool block) const
+{
+}
+
+void ContextPropertyPrivate::ignoreCommander()
+{
+}
+
+void ContextPropertyPrivate::setTypeCheck(bool typeCheck)
+{
+}
+
 
 ContextProperty::ContextProperty(const QString &key, QObject *parent)
     : QObject(parent)
@@ -69,177 +407,3 @@ void ContextProperty::setTypeCheck(bool typeCheck)
 #if QT_VERSION < 0x050000
 void ContextProperty::onValueChanged() { }
 #endif
-
-ContextPropertyPrivate::ContextPropertyPrivate(const QString &key, QObject *parent)
-    : QObject(parent)
-    , key_(key)
-    , file_(getStateFsPath(key))
-    , notifier_(nullptr)
-    , reopen_interval_(100)
-    , reopen_timer_(new QTimer(this))
-    , is_subscribed_(false)
-    , is_cached_(false)
-{
-    reopen_timer_->setSingleShot(true);
-    connect(reopen_timer_, SIGNAL(timeout()), this, SLOT(trySubscribe()));
-    subscribe();
-}
-
-ContextPropertyPrivate::~ContextPropertyPrivate()
-{
-    unsubscribe();
-}
-
-QString ContextPropertyPrivate::key() const
-{
-    return key_;
-}
-
-void ContextPropertyPrivate::trySubscribe() const
-{
-    if (tryOpen()) {
-        reopen_interval_ = 100;
-        return subscribe();
-    }
-
-    reopen_interval_ *= 2;
-    if (reopen_interval_ > 1000 * 60 * 3)
-        reopen_interval_ = 1000 * 60 * 3;
-
-    reopen_timer_->start(reopen_interval_);
-}
-
-void ContextPropertyPrivate::resubscribe() const
-{
-    bool was_subscribed = is_subscribed_;
-    unsubscribe();
-
-    if (was_subscribed)
-        subscribe();
-}
-
-QVariant ContextPropertyPrivate::value(const QVariant &defVal) const
-{
-    QVariant res(defVal);
-
-    if (is_cached_)
-        return cache_;
-
-    if (!tryOpen()) {
-        qWarning() << "Can't open " << file_.fileName();
-        return res;
-    }
-
-    // WORKAROUND: file is just opened and closed before reading from
-    // real source to make vfs (?) reread file data to cache
-    QFile touchFile(file_.fileName());
-    touchFile.open(QIODevice::ReadOnly | QIODevice::Unbuffered);
-
-    file_.seek(0);
-    auto size = file_.size();
-    if (buffer_.size() < size)
-        buffer_.resize(size + 8);
-
-    int rc = file_.read(buffer_.data(), size + 7);
-    touchFile.close();
-    if (rc >= 0) {
-        buffer_[rc] = '\0';
-        auto s = QString(buffer_);
-        if (s.size()) // use read data if not empty
-            res = cKitValueDecode(s);
-
-        cache_ = res;
-        is_cached_ = true;
-
-        if (notifier_)
-            notifier_->setEnabled(true);
-    } else {
-        qWarning() << "Error accessing? " << rc << "..." << file_.fileName();
-        resubscribe();
-    }
-    return res;
-}
-
-QVariant ContextPropertyPrivate::value() const
-{
-    return value(QVariant());
-}
-
-const ContextPropertyInfo* ContextPropertyPrivate::info() const
-{
-    return nullptr; // TODO
-}
-
-void ContextPropertyPrivate::handleActivated(int)
-{
-    if (notifier_)
-        notifier_->setEnabled(false);
-    is_cached_ = false;
-    emit valueChanged();
-}
-
-bool ContextPropertyPrivate::tryOpen() const
-{
-    if (file_.isOpen())
-        return true;
-
-    if (!file_.exists()) {
-        qWarning() << "Property file " << file_.fileName()
-                   << " does not exist";
-        return false;
-    }
-    file_.open(QIODevice::ReadOnly | QIODevice::Unbuffered);
-    if (!file_.isOpen()) {
-        qWarning() << "Can't open " << file_.fileName();
-        return false;
-    }
-    is_cached_ = false;
-    return true;
-}
-
-void ContextPropertyPrivate::subscribe() const
-{
-    if (is_subscribed_)
-        return;
-
-    is_subscribed_ = true;
-    if (!tryOpen())
-        return reopen_timer_->start(reopen_interval_);
-
-    notifier_.reset(new QSocketNotifier(file_.handle(), QSocketNotifier::Read));
-    connect(notifier_.data(), SIGNAL(activated(int))
-            , this, SLOT(handleActivated(int)));
-    notifier_->setEnabled(true);
-}
-
-void ContextPropertyPrivate::unsubscribe() const
-{
-    if (!is_subscribed_)
-        return;
-
-    is_subscribed_ = false;
-    if (!file_.isOpen())
-        return;
-
-    if (notifier_) {
-        notifier_->setEnabled(false);
-        notifier_.reset();
-    }
-    file_.close();
-}
-
-void ContextPropertyPrivate::waitForSubscription() const
-{
-}
-
-void ContextPropertyPrivate::waitForSubscription(bool block) const
-{
-}
-
-void ContextPropertyPrivate::ignoreCommander()
-{
-}
-
-void ContextPropertyPrivate::setTypeCheck(bool typeCheck)
-{
-}
--- src/property.hpp
+++ src/property.hpp
@@ -1,16 +1,158 @@
 #ifndef _STATEFS_CKIT_PROPERTY_HPP_
 #define _STATEFS_CKIT_PROPERTY_HPP_
 
+#include <functional>
+
 #include <QObject>
 #include <QString>
 #include <QVariant>
 #include <QFile>
 #include <QScopedPointer>
 #include <QByteArray>
+#include <QWaitCondition>
+#include <QThread>
+#include <QMutex>
+#include <QCoreApplication>
+#include <QSet>
+#include <QMap>
+#include <QSocketNotifier>
 
 class ContextPropertyInfo;
 class QSocketNotifier;
 class QTimer;
+
+
+class ContextPropertyPrivate;
+
+namespace ckit {
+
+
+class CKitProperty : public QObject
+{
+    Q_OBJECT;
+public:
+    CKitProperty(QString const &key, QObject *parent);
+    virtual ~CKitProperty();
+
+signals:
+    void changed(QVariant);
+
+private slots:
+    void handleActivated(int);
+    void trySubscribe() const;
+
+private:
+    bool tryOpen() const;
+    void resubscribe() const;
+
+    QVariant value() const;
+    void subscribe () const;
+    void unsubscribe () const;
+
+    QString key_;
+    mutable QFile file_;
+    mutable QScopedPointer<QSocketNotifier> notifier_;
+    mutable QByteArray buffer_;
+    mutable int reopen_interval_;
+    mutable QTimer *reopen_timer_;
+    mutable bool is_subscribed_;
+    mutable bool is_cached_;
+    mutable QVariant cache_;
+};
+
+class Actor_ : public QThread
+{
+    Q_OBJECT;
+protected:
+    Actor_(QObject *parent) : QThread(parent) {}
+    virtual ~Actor_() {}
+};
+
+
+template <typename T>
+class Actor : public Actor_
+{
+public:
+    Actor(std::function<T*()> ctor, QObject *parent = nullptr)
+        : Actor_(parent)
+        , ctor_(ctor)
+    {
+        mutex_.lock();
+        start();
+        cond_.wait(&mutex_);
+        mutex_.unlock();
+    }
+
+    void run()
+    {
+        obj_.reset(ctor_());
+        mutex_.lock();
+        cond_.wakeAll();
+        mutex_.unlock();
+        exec();
+        obj_.reset(nullptr);
+    }
+
+    inline void postEvent(QEvent *e)
+    {
+        QCoreApplication::postEvent(obj_.data(), e);
+    }
+
+    inline bool sendEvent(QEvent const *e)
+    {
+        return QCoreApplication::sendEvent(obj_.data(), e);
+    }
+
+private:
+    QWaitCondition cond_;
+    QMutex mutex_;
+    std::function<T*()> ctor_;
+    QScopedPointer<T> obj_;
+};
+
+class PropertyMonitor : public QObject
+{
+    Q_OBJECT;
+public:
+    virtual bool event(QEvent *);
+private:
+    void subscribe(ContextPropertyPrivate const*, const QString &);
+    void unsubscribe(ContextPropertyPrivate const*, const QString &);
+    CKitProperty *add(const QString &);
+
+    QMap<QString, QSet<ContextPropertyPrivate const*> > targets_;
+    QMap<QString, CKitProperty*> properties_;
+};
+
+class Event : public QEvent
+{
+public:
+    enum Type {
+        Subscribe = QEvent::User,
+        Unsubscribe
+    };
+
+    virtual ~Event();
+
+protected:
+    Event(Type t);
+private:
+    Event();
+
+};
+
+class PropertyRequest : public Event
+{
+public:
+    PropertyRequest(Event::Type, ContextPropertyPrivate const*, QString const&);
+    virtual ~PropertyRequest();
+
+    ContextPropertyPrivate const *tgt_;
+    QString key_;
+};
+
+
+}
 
 class ContextPropertyPrivate : public QObject
 {
@@ -35,25 +177,16 @@ public:
 
     static void ignoreCommander();
     static void setTypeCheck(bool typeCheck);
-
 signals:
+
     void valueChanged();
 
-private slots:
-    void handleActivated(int);
-    void trySubscribe() const;
-
+public slots:
+    void changed(QVariant);
 private:
 
-    bool tryOpen() const;
-    void resubscribe() const;
-
+    static ckit::Actor<ckit::PropertyMonitor> *actor();
     QString key_;
-    mutable QFile file_;
-    mutable QScopedPointer<QSocketNotifier> notifier_;
-    mutable QByteArray buffer_;
-    mutable int reopen_interval_;
-    mutable QTimer *reopen_timer_;
     mutable bool is_subscribed_;
     mutable bool is_cached_;
     mutable QVariant cache_;
